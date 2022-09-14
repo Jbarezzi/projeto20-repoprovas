5648811ca70e877e07b9ad3ae33f606d92cbb947
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-14 20:40:39 -0300

    refactor: separate server from file

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import "express-async-errors";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routers";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(router);
+
+export default app;
