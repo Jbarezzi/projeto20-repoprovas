40c0cd363d4a47d83a84bfac3fa4fbb2cc721838
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-14 21:56:27 -0300

    feat: implement auth repositories

---
+++ src/repositories/authRepository.ts
@@ -0,0 +1,15 @@
+import client from "../database/prisma";
+import { ISign } from "../interfaces/authTypes";
+
+export async function findByEmail(email: string) {
+  const user = await client.user.findUnique({ where: { email } });
+  return user;
+}
+
+export async function insert(newUser: ISign) {
+  delete newUser.confirmPassword;
+  const result = await client.user.create({ data: newUser });
+  if (result === null) {
+    throw { type: "error" };
+  }
+}
