c442c65eb0f7fd3ecae7cbc9c79986e08a172cc7
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-14 21:36:00 -0300

    fix: change confirmPassword schema

--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -3,5 +3,5 @@ import Joi from "joi";
 export const signupSchema = Joi.object({
   email: Joi.string().email().trim().required(),
   password: Joi.string().trim().required(),
-  confirmPassword: Joi.ref("password"),
+  confirmPassword: Joi.string().trim().required(),
 });
