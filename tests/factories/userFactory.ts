a733644dab8990809335b0de8f6723b5c0405dc2
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-15 16:07:44 -0300

    feat: implement tests

--- tests/factories/userFactory.ts
+++ tests/factories/userFactory.ts
@@ -1,8 +1,35 @@
-import { randEmail } from "@ngneat/falso";
+import { randEmail, randUser } from "@ngneat/falso";
 
-export function createUser() {
+export function __createUser() {
   const user = {
     email: randEmail(),
+    password: "1234",
+    confirmPassword: "1234",
+  };
+  return user;
+}
+
+export function __createExistentUser() {
+  const user = {
+    email: "test@test.com",
+    password: "1234",
+    confirmPassword: "1234",
+  };
+  return user;
+}
+
+export function __createUserWithMismatchedPasswords() {
+  const user = {
+    email: randEmail(),
+    password: "123",
+    confirmPassword: "1234",
+  };
+  return user;
+}
+
+export function __createUserWithInvalidEmail() {
+  const user = {
+    email: randUser().username,
     password: "1234",
     confirmPassword: "1234",
   };
--- tests/user.test.ts
+++ tests/user.test.ts
@@ -4,9 +4,37 @@ import { userFactory } from "./factories";
 
 describe("POST /signup", () => {
   it("given a valid user should return 201", async () => {
-    const body = userFactory.createUser();
+    const body = userFactory.__createUser();
     const result = await supertest(app).post("/signup").send(body);
     const status = result.status;
     expect(status).toEqual(201);
+  });
+
+  it("given a valid user that already exists should return 409", async () => {
+    const body = userFactory.__createExistentUser();
+    const result = await supertest(app).post("/signup").send(body);
+    const status = result.status;
+    expect(status).toEqual(409);
+  });
+
+  it("given password and confirmPassword not matching should return 409", async () => {
+    const body = userFactory.__createUserWithMismatchedPasswords();
+    const result = await supertest(app).post("/signup").send(body);
+    const status = result.status;
+    expect(status).toEqual(409);
+  });
+
+  it("given a empty body should return 422", async () => {
+    const body = {};
+    const result = await supertest(app).post("/signup").send(body);
+    const status = result.status;
+    expect(status).toEqual(422);
+  });
+
+  it("given a invalid email should return 422", async () => {
+    const body = userFactory.__createUserWithInvalidEmail();
+    const result = await supertest(app).post("/signup").send(body);
+    const status = result.status;
+    expect(status).toEqual(422);
   });
 });
