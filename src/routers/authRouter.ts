70d5224b598007ba98d156b54f0151515c1bcec9
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-16 11:04:54 -0300

    feat: implement signin router

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { joiValidator } from "../middlewares";
-import { signupSchema } from "../schemas";
+import { authSchemas } from "../schemas";
 import { authController } from "./../controllers";
 
 export const authRouter = Router();
 
-authRouter.post("/signup", joiValidator(signupSchema), authController.signup);
+authRouter.post(
+  "/signup",
+  joiValidator(authSchemas.signupSchema),
+  authController.signup
+);
+authRouter.post(
+  "/signin",
+  joiValidator(authSchemas.signinSchema),
+  authController.signin
+);
