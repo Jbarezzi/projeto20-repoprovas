4ee1d4e50420b78bd21f3169c418aaec2a7e8242
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-18 14:36:18 -0300

    Truncate heroku db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,34 +2,6 @@ import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient();
 
 async function main() {
-  await client.$executeRaw`INSERT INTO terms ("number") VALUES (1);`;
-  await client.$executeRaw`INSERT INTO terms ("number") VALUES (2);`;
-  await client.$executeRaw`INSERT INTO terms ("number") VALUES (3);`;
-  await client.$executeRaw`INSERT INTO terms ("number") VALUES (4);`;
-  await client.$executeRaw`INSERT INTO terms ("number") VALUES (5);`;
-  await client.$executeRaw`INSERT INTO terms ("number") VALUES (6);`;
-
-  await client.$executeRaw`INSERT INTO categories ("name") VALUES ('Projeto');`;
-  await client.$executeRaw`INSERT INTO categories ("name") VALUES ('Prática');`;
-  await client.$executeRaw`INSERT INTO categories ("name") VALUES ('Recuperação');`;
-
-  await client.$executeRaw`INSERT INTO teachers ("name") VALUES ('Diego Pinho');`;
-  await client.$executeRaw`INSERT INTO teachers ("name") VALUES ('Bruna Hamori');`;
-
-  await client.$executeRaw`INSERT INTO disciplines ("name", "termId") VALUES ('HTML e CSS', 1);`;
-  await client.$executeRaw`INSERT INTO disciplines ("name", "termId") VALUES ('JavaScript', 2);`;
-  await client.$executeRaw`INSERT INTO disciplines ("name", "termId") VALUES ('React', 3);`;
-  await client.$executeRaw`INSERT INTO disciplines ("name", "termId") VALUES ('Humildade', 1);`;
-  await client.$executeRaw`INSERT INTO disciplines ("name", "termId") VALUES ('Planejamento', 2);`;
-  await client.$executeRaw`INSERT INTO disciplines ("name", "termId") VALUES ('Autoconfiança', 3);`;
-
-  await client.$executeRaw`INSERT INTO "teachersDisciplines" ("teacherId", "disciplineId") VALUES (1, 1);`;
-  await client.$executeRaw`INSERT INTO "teachersDisciplines" ("teacherId", "disciplineId") VALUES (1, 2);`;
-  await client.$executeRaw`INSERT INTO "teachersDisciplines" ("teacherId", "disciplineId") VALUES (1, 3);`;
-  await client.$executeRaw`INSERT INTO "teachersDisciplines" ("teacherId", "disciplineId") VALUES (2, 4);`;
-  await client.$executeRaw`INSERT INTO "teachersDisciplines" ("teacherId", "disciplineId") VALUES (2, 5);`;
-  await client.$executeRaw`INSERT INTO "teachersDisciplines" ("teacherId", "disciplineId") VALUES (2, 6);`;
-
   await client.$executeRaw`TRUNCATE TABLE users RESTART IDENTITY CASCADE`;
   await client.$executeRaw`TRUNCATE TABLE categories RESTART IDENTITY CASCADE`;
   await client.$executeRaw`TRUNCATE TABLE disciplines RESTART IDENTITY CASCADE`;
